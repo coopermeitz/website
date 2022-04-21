cbbafb2e5fc6fc1589effec43a569bb7b8d17a6b
Cooper Meitz <coopermeitz@gatech.edu>
2022-04-21 14:44:07 -0400

    Basic Vue webpage

---
+++ src/components/Tabs.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1> Hey </h1>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    name: 'page-tabs'
+})
+</script>
+
+
+<style scoped>
+
+</style>
